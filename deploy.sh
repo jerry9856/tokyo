29a2aa879a92d18989bcf71fd08e0f8f3545c33b
jerry <jaskk870506@gmail.com>
2025-11-21 11:02:08 +0800

    可帶入參數

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,16 @@ VERSION="v.$(date +'%Y.%m.%d.%H%M')"
 # Works on macOS (sed -i '')
 sed -i '' "s/v\.[0-9]\{4\}\.[0-9]\{2\}\.[0-9]\{2\}\.[0-9]\{4\}/$VERSION/g" index.html
 
+# Get commit message from parameter or use default
+if [ -n "$1" ]; then
+    COMMIT_MSG="$1"
+else
+    COMMIT_MSG="Update version to $VERSION"
+fi
+
 # Add the file, commit, and push
 git add index.html
-git commit -m "Update version to $VERSION"
+git commit -m "$COMMIT_MSG"
 git push
 
-echo "Deployed version $VERSION"
+echo "Deployed version $VERSION with message: $COMMIT_MSG"
